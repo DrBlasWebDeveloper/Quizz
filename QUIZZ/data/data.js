79e5ccb8352a8b06e8eb01ccaebc9b846feb97b2
Blas Alonso Barbosa López <94561240+DrBlasWebDeveloper@users.noreply.github.com>
2022-04-14 22:48:12 -0500

    Add files via upload

---
+++ QUIZZ/data/data.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    question: "Es el concepto oficial de Salud",
+    choices: ["Es la auscencia de toda enfermedad", "Es estar bien fisica, social y emocionalmente", "No tener enfermedades", "Completo Bienestar Fisico, social y emocional y no solo la ausencia de la enfermedad"],
+    answer: "Completo Bienestar Fisico, social y emocional y no solo la ausencia de la enfermedad",
+  },
+  {
+    question: "Ejemplo de Síntomas, Excepto:",
+    choices: ["Nauseas", "Mareo", "Dolor de Cabeza", "Frecuencia Cardíaca"],
+    answer: "Frecuencia Cardíaca",
+  },
+  {
+    question: "Inflamación del Estómago",
+    choices: ["Estomatitis", "Enteritis", "Gastritis", "Disfagia", 'Disnea'],
+    answer: "Gastritis",
+  },
+  {
+    question: "Día Mundial de la Salud",
+    choices: ["28 de Mayo", "7 de Abril", "12 Septiembre", "12 Mayo"],
+    answer: "7 de Abril",
+  },
+  {
+    question: "Que es el Triage",
+    choices: ["Sistema de clasificación de los pacientes dependiento el tiempo que llevan esperando consulta", "Sistema de clasificación de los pacientes dependiendo el día que sacaron cita", "Sistema de clasificación de los pacientes con base a su estado de salud", "Da prioridad a los pacientes estables"],
+    answer: "Sistema de clasificación de los pacientes con base a su estado de salud",
+  },
+  {
+    question: "Una cifra de Presión Arterial en 160/80mmhg",
+    choices: ["Hipotensión Arterial", "Hipertensión Arterial Sistólica", "Normal", "Hipertensión Arterial Diastólica"],
+    answer: "Hipertensión Arterial Sistólica",
+  },
+  {
+    question: "Es la ruptura parcial de los ligamentos y se requiere reposo e inmovilización por semanas",
+    choices: ["Esguince de 1er Grado", "Esguince indirecto", "Esguince de 3er Grado", "Esguince de 2do Grado"],
+    answer: "Esguince de 2do Grado",
+  },
+  {
+    question: "Paciente que No tiene Fiebre o Sin fiebre",
+    choices: ["Febril", "Hipertermia", "Afebril", "Hipotermia"],
+    answer: "Afebril",
+  },
+  {
+    question: "Son los apartados de la Historia Clínica, excepto:",
+    choices: ["Antecedentes Personales Patológicos", "Ficha de Identificación", "Antecedentes Quirúrgicos", "Antecedentes Personales Patológicos"],
+    answer: "Antecedentes Quirúrgicos",
+  },
+  {
+    question: "Conocida como la Pionera de la Enfermería o la Dama de la Lampara",
+    choices: ["Hipocrates", "Laura Neigser", "Aristoteles", 'Atenea', 'Florence Nightingale'],
+    answer: "Florence Nightingale",
+  },
+];
